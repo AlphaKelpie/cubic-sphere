6a708efeb4650aeb89d2b00fb7f8862bb421ea4b
Tommaso Rondini <tommaso.rondini@studio.unibo.it>
2026-06-10 09:43:00 +0200

    class for random noise analysis on 3d

---
+++ grid3.cpp
@@ -0,0 +1,256 @@
+#include "grid3.h"
+
+Grid3::Grid3(Interval x, Interval y, Interval z)
+    : _nPoints{0},
+      _rho{nullptr},
+      _projection{nullptr},
+      _neighbour{nullptr},
+      _h{0.1},
+      _delta{1.1 * sqrt(3) * _h}
+{
+    int xPoints = (x.max - x.min) / _h;
+    int yPoints = (y.max - y.min) / _h;
+    int zPoints = (z.max - z.min) / _h;
+    _nPoints = xPoints * yPoints * zPoints;
+
+    // int spoints = 0;
+    // Create surface and get _nPoints
+    Point* temp = new Point[_nPoints];
+    // Point* temp_s = new Point[_nPoints];
+    _neighbour = new Neighbours[_nPoints];
+
+    //for every point of the descrete domain..
+    int bottom;
+    int top;
+    int behind;
+    int front;
+    int right;
+    int left;
+    for (double i = x.min; i < x.max; i+=_h) {
+        left = (i <= x.min) ? -1 : 1;
+        right = (i >= x.max - _h) ? -1 : 1;
+        for (double j = y.min; j < y.max; j+=_h) {
+            front = (j <= y.min) ? -1 : 1;
+            behind = (j >= y.max - _h) ? -1 : 1;
+            for (double k = z.min; k < z.max; k+=_h) {
+                bottom = (k <= z.min) ? -1 : 1;
+                top = (k >= z.max - _h) ? -1 : 1;
+                //..add it to the volume and..
+                temp[_nPoints] = {i, j, k};
+                //..add it to the surface if it's inside the narrow-band domain..
+                // double dist = phi(i,j,k);
+                // if (dist > -_delta && dist < _delta) {
+                //     temp_s[spoints] = {i,j,k};
+                //     ++spoints;
+                // }
+                //..create corresponding neighbour
+                _neighbour[_nPoints].point[0] = _nPoints;   // itself
+                _neighbour[_nPoints].point[1] = std::max((_nPoints + 1) * top, -1);
+                _neighbour[_nPoints].point[2] = std::max(abs(_nPoints - 1) * bottom, -1);
+                _neighbour[_nPoints].point[3] = std::max((_nPoints + zPoints) * behind, -1);
+                _neighbour[_nPoints].point[4] = std::max(abs(_nPoints - zPoints) * front, -1);
+                _neighbour[_nPoints].point[5] = std::max((_nPoints + yPoints * zPoints) * right, -1);
+                _neighbour[_nPoints].point[6] = std::max(abs(_nPoints - yPoints * zPoints) * left, -1);
+                ++_nPoints;
+            }
+        }
+    }
+    _volume = Surface(_nPoints, temp);
+    // _sphere = Surface(spoints, temp_s);
+    delete[] temp;
+    // delete[] temp_s;
+
+    createProjection();
+}
+
+Grid3::Grid3(std::string mapFile)
+    : _nPoints{0},
+      _rho{nullptr},
+      _projection{nullptr},
+      _volume{Surface()}
+    //   _sphere{Surface()}
+{
+    if (!loadMap(mapFile))
+        throw std::runtime_error{"loadMap() cannot open file."};
+}
+
+Grid3::~Grid3() {
+    delete[] _rho;
+    delete[] _projection;
+    delete[] _neighbour;
+}
+
+void Grid3::saveRelations(std::string filename) {
+    //format: 1 row _h and _nPoints
+    // 1 row per point to _projection: index and weight for the 8 grid
+    // points around
+    // 1 row per point to _neighbour indexes: itself top bottom behind
+    // front right left
+
+    std::cout << "Saving h, nPoints, projection and neighbour...     ";
+    std::fstream fout(filename, std::ios::out);
+
+    fout << _h << ' ' << _nPoints << "\n\n";
+    
+    for (int i = 0; i < _nPoints; ++i) {
+        for (int j = 0; j < 8; ++j) {
+            fout << _projection[i].first[j] << ' ';
+            fout << _projection[i].second[j] << ' ';
+        }
+        fout << "\n";
+    }
+
+    fout << "\n";
+
+    for (int i = 0; i < _nPoints; ++i) {
+        for (int j = 0; j < 7; ++j) {
+            fout << _neighbour[i].point[j] << ' ';
+        }
+        fout << "\n";
+    }
+
+    fout.close();
+    std::cout << "Done!\n";
+}
+
+void Grid3::saveSurface(std::string filename) {
+  std::cout << "Saving surface...     ";
+  if (_volume.nPoints() == 0) {
+    std::cerr << "Error, cannot save surface of a simulation created from a map.\n";
+    return;
+  }
+
+  std::fstream fout(filename, std::ios::out);
+  fout << _volume;
+  fout.close();
+
+  std::cout << "Done!\n";
+}
+
+void Grid3::saveRho(std::string filename) {
+  std::fstream fout(filename, std::ios::out);
+  for (int i = 0; i < _nPoints; ++i)
+    fout << _rho[i] << '\n';
+
+  fout.close();
+}
+
+void Grid3::evolve(double dt = 0) {
+
+}
+
+void Grid3::project() {
+
+}
+
+// private
+
+double Grid3::phi(double x, double y, double z) {
+    //default function: a sphere centred in (5,5,5) with r=4.5
+    return std::sqrt((x-5)*(x-5) + (y-5)*(y-5) + (z-5)*(z-5)) - 4.5;
+}
+
+void Grid3::createProjection() {
+    auto start_time = std::chrono::high_resolution_clock::now();
+    std::cout << "Constructing map...     ";
+    _projection = new std::pair<int[8], double[8]>[_nPoints];
+    for (int i = 0; i < _nPoints; ++i) {
+        Point c = closest(_volume[i]);
+        
+        //(A)  find the indexes of the 8 surface points surrounding the closest point of point[i]:
+        //(A1) find the points (actually, only the left-front-bottom)
+        double x0 = (int)(c.x/_h) *_h;
+        double y0 = (int)(c.y/_h) *_h;
+        double z0 = (int)(c.z/_h) *_h;
+
+        //(A2) find their indexes in _volume
+        for (int k = 0; k < _nPoints; ++k) {
+            Point s = _volume[k];
+            
+            if      (s == Point{x0,    y0,    z0   })   _projection[i].first[0] = k;
+            else if (s == Point{x0,    y0,    z0+_h})   _projection[i].first[1] = k;
+            else if (s == Point{x0,    y0+_h, z0   })   _projection[i].first[2] = k;
+            else if (s == Point{x0,    y0+_h, z0+_h})   _projection[i].first[3] = k;
+            else if (s == Point{x0+_h, y0,    z0   })   _projection[i].first[4] = k;
+            else if (s == Point{x0+_h, y0,    z0+_h})   _projection[i].first[5] = k;
+            else if (s == Point{x0+_h, y0+_h, z0   })   _projection[i].first[6] = k;
+            else if (s == Point{x0+_h, y0+_h, z0+_h})   _projection[i].first[7] = k;
+        }
+
+        //(B) find the weights of the interpolation
+        double xd = (c.x - x0) /_h;
+        double yd = (c.y - y0) /_h;
+        double zd = (c.z - z0) /_h;
+
+        _projection[i].second[0] = (1-xd) * (1-yd) * (1-zd);
+        _projection[i].second[1] = (1-xd) * (1-yd) *   zd;
+        _projection[i].second[2] = (1-xd) *   yd   * (1-zd);
+        _projection[i].second[3] = (1-xd) *   yd   *   zd;
+        _projection[i].second[4] =   xd   * (1-yd) * (1-zd);
+        _projection[i].second[5] =   xd   * (1-yd) *   zd;
+        _projection[i].second[6] =   xd   *   yd   * (1-zd);
+        _projection[i].second[7] =   xd   *   yd   *   zd;
+    }
+    auto end_time = std::chrono::high_resolution_clock::now();
+    auto duration = std::chrono::duration_cast<std::chrono::minutes>(end_time - start_time);
+    std::cout << "Done! (" << duration.count() << " minutes)\n";
+}
+
+Point Grid3::closest(Point const& p) {
+
+  double x = p.x;
+  double y = p.y;
+  double z = p.z;
+
+  double gradient[3];   //nabla_h (phi)
+  gradient[0] = (phi(x+_h, y   , z   ) - phi(x-_h, y,    z   )) /(2*_h);
+  gradient[1] = (phi(x,    y+_h, z   ) - phi(x,    y-_h, z   )) /(2*_h);
+  gradient[2] = (phi(x,    y,    z+_h) - phi(x,    y,    z-_h)) /(2*_h);
+
+  //if gradient = 0, the point is already on the surface
+  if (gradient[0] == 0 && gradient[1] == 0 && gradient[2] == 0) {
+    return p;
+  }
+
+  // | nabla_h (phi) |
+  double mod = std::sqrt(gradient[0]*gradient[0] + gradient[1]*gradient[1] + gradient[2]*gradient[2]);
+
+  double dist_xyz = phi(x,y,z);
+
+  Point r;
+  r.x = x - gradient[0] * dist_xyz / mod;
+  r.y = y - gradient[1] * dist_xyz / mod;
+  r.z = z - gradient[2] * dist_xyz / mod;
+
+  return r;
+}
+
+bool Grid3::loadMap(std::string filename) {
+    std::cout << "Loading map...     ";
+    std::fstream fin(filename, std::ios::in);
+    if (!fin.is_open()) {
+        std::cerr << "Error. Projection file '"<< filename << "' couldn't be opened.\n";
+        return false;
+    }
+
+    fin >> _h;
+    fin >> _nPoints;
+
+    _projection = new std::pair<int[8], double[8]>[_nPoints];
+    for (int i = 0; i < _nPoints; ++i) {
+        for (int j = 0; j < 8; ++j) {
+            fin >> _projection[i].first[j];
+            fin >> _projection[i].second[j];
+        }
+    }
+
+    _neighbour = new Neighbours[_nPoints];
+    for (int i = 0; i < _nPoints; ++i) {
+        for (int j = 0; j < 7; ++j) {
+            fin >> _neighbour[i].point[j];
+        }
+    }
+
+    std::cout << "Done.\n";
+    return true;
+}
---
+++ grid3.h
@@ -0,0 +1,59 @@
+#ifndef GRID3_H
+#define GRID3_H
+
+#include "surface.h"
+#include <algorithm>
+#include <chrono>
+#include <fstream>
+#include <iostream>
+
+class Grid3 {
+    private:
+    // Neighbours structure
+    struct Neighbours {
+        int point[7];
+    };
+
+    // num of points
+    int _nPoints;
+    // points in the volume
+    Surface _volume;
+    // points on the surface
+    // Surface _sphere;
+    // density
+    double* _rho;
+
+    // 8 grid points around projection of each point:
+    // first: array of index in _s of the points
+    // second: array of weights
+    std::pair<int[8], double[8]>* _projection;
+
+    // 7 grid points around each point and itself
+    Neighbours* _neighbour;
+
+    double _h;
+    double _delta;
+
+    public:
+    Grid3(Interval x = {0,10}, Interval y = {0,10}, Interval z = {0,10});
+    Grid3(std::string mapFile);
+    ~Grid3();
+
+    void saveRelations(std::string filename = "relation.dat");
+    void saveSurface(std::string filename = "surface.dat");
+    void saveRho(std::string filename = "rho.dat");
+
+    void evolve(double dt = 0);
+    void project();
+
+    private:
+    double phi(double x, double y, double z);
+    void createProjection();
+    //return the point on the surface which is closest to given point
+    Point closest(Point const& p);
+    //load the map between each surface point 'p' and the interpolation(closest(neighbour(p))).
+    //Returns false if file is not found.
+    bool loadMap(std::string filename = "map.dat");
+};
+
+#endif
